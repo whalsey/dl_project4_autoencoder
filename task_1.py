7581082eb8987364322454df050e51661e702deb
William Halsey <admiral@whalsey.nomad.utk.edu>
2018-10-29 12:19:53 -0400

    added more layers

--- task_1.py
+++ task_1.py
@@ -10,7 +10,7 @@ cur_dir = os.path.curdir
 
 class my_autoencoder:
 
-    def __init__(self, sess=None, latent_dim=2048, learning_rate=1e-4, epochs=50, batch_size=100, data=None, task=None, restore=False, restore_path=None):
+    def __init__(self, sess=None, latent_dim=512, learning_rate=1e-4, epochs=50, batch_size=100, data=None, task=None, restore=False, restore_path=None):
         self.sess = sess
         self.latent_dim = latent_dim
         self.learning_rate = learning_rate
@@ -38,20 +38,40 @@ class my_autoencoder:
         self.x = tf.placeholder(tf.float32, [None, 32, 32, 3], name='input_layer')
         self.y_ = tf.placeholder(tf.float32, [None, 32, 32, 3], name='ground_truth')
 
-        with tf.name_scope('Encoder'):
+        with tf.name_scope('Encoder_1'):
             flat = tf.reshape(self.x, [-1, 3072])
-            self.weights = tf.Variable(tf.truncated_normal([3072, self.latent_dim]), name='weights')
-            self.enc_bias = tf.Variable(tf.truncated_normal([self.latent_dim]), name='end_bias')
+            self.weights_1 = tf.Variable(tf.truncated_normal([3072, 2048]), name='weights_1')
+            self.enc1_bias = tf.Variable(tf.truncated_normal([2048]), name='enc1_bias')
 
-            encoded = tf.nn.relu(tf.add(tf.matmul(flat, self.weights), self.enc_bias), name='encoded')
+            self.encoded_1 = tf.nn.relu(tf.add(tf.matmul(flat, self.weights_1), self.enc1_bias), name='encoded_1')
 
-        with tf.name_scope('Decoder'):
-            self.dec_bias = tf.Variable(tf.truncated_normal([3072]), name='dec_bias')
-            decoded = tf.nn.relu(tf.add(tf.matmul(encoded, tf.transpose(self.weights)), self.dec_bias), name='decoded')
+        with tf.name_scope('Encoder_2'):
+            self.weights_2 = tf.Variable(tf.truncated_normal([2048, 1024]), name='weights_2')
+            self.enc2_bias = tf.Variable(tf.truncated_normal([1024]), name='enc2_bias')
 
-            self.y = tf.reshape(decoded, [-1, 32, 32, 3], name='output')
+            self.encoded_2 = tf.nn.relu(tf.add(tf.matmul(flat, self.weights_2), self.enc2_bias), name='encoded_2')
 
-            self.loss = tf.reduce_mean(tf.pow(tf.subtract(self.y_, self.y), 2))
+        with tf.name_scope('Encoder_3'):
+            self.weights_3 = tf.Variable(tf.truncated_normal([1024, self.latent_dim]), name='weights_3')
+            self.enc3_bias = tf.Variable(tf.truncated_normal([self.latent_dim]), name='enc3_bias')
+
+            encoded = tf.nn.relu(tf.add(tf.matmul(flat, self.weights_3), self.enc3_bias), name='encoded_3')
+
+        with tf.name_scope('Decoder_3'):
+            self.dec3_bias = tf.Variable(tf.truncated_normal([1024]), name='dec3_bias')
+            self.decoded_3 = tf.nn.relu(tf.add(tf.matmul(encoded, tf.transpose(self.weights_3)), self.dec3_bias), name='decoded_3')
+
+        with tf.name_scope('Decoder_2'):
+            self.dec2_bias = tf.Variable(tf.truncated_normal([2048]), name='dec2_bias')
+            self.decoded_2 = tf.nn.relu(tf.add(tf.matmul(self.decoded_3, tf.transpose(self.weights_2)), self.dec2_bias), name='decoded_2')
+
+        with tf.name_scope('Decoder_1'):
+            self.dec1_bias = tf.Variable(tf.truncated_normal([3072]), name='dec1_bias')
+            decoded = tf.nn.relu(tf.add(tf.matmul(self.decoded_2, tf.transpose(self.weights_1)), self.dec1_bias), name='decoded_1')
+
+        self.y = tf.reshape(decoded, [-1, 32, 32, 3], name='output')
+
+        self.loss = tf.reduce_mean(tf.pow(tf.subtract(self.y_, self.y), 2))
 
         self.train_step = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.loss)
 
